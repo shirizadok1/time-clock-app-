3019c961b0912d7f90e9b451c4b56899585c1983
שמעון בוזגלו <shirizadok1@gmail.com>
2023-03-16 13:49:26 +0200

    styling was added to employeeDash

--- src/components/EmployeeDash.tsx
+++ src/components/EmployeeDash.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 interface Report {
   id: number;
@@ -18,12 +18,12 @@ const EmployeeDash = () => {
   const [showReport, setShowReport] = useState(false);
 
   useEffect(() => {
-    const monthlyReportFromStorage = localStorage.getItem('monthlyReport');
+    const monthlyReportFromStorage = localStorage.getItem("monthlyReport");
     if (monthlyReportFromStorage) {
       setMonthlyReport(JSON.parse(monthlyReportFromStorage));
     } else {
       axios
-        .get<Report[]>('/api/data.json')
+        .get<Report[]>("/api/data.json")
         .then((res) => setMonthlyReport(res.data))
         .catch((err) => console.log(err));
     }
@@ -32,72 +32,80 @@ const EmployeeDash = () => {
   const handleStartEdit = (reportIndex: number, dayIndex: number) => {
     const updatedMonthlyReport = [...monthlyReport];
     updatedMonthlyReport[reportIndex].hours[dayIndex].start = new Date();
-    localStorage.setItem('monthlyReport', JSON.stringify(updatedMonthlyReport));
+    localStorage.setItem("monthlyReport", JSON.stringify(updatedMonthlyReport));
     setMonthlyReport(updatedMonthlyReport);
   };
 
   const handleStopEdit = (reportIndex: number, dayIndex: number) => {
     const updatedMonthlyReport = [...monthlyReport];
     updatedMonthlyReport[reportIndex].hours[dayIndex].end = new Date();
-    localStorage.setItem('monthlyReport', JSON.stringify(updatedMonthlyReport));
+    localStorage.setItem("monthlyReport", JSON.stringify(updatedMonthlyReport));
     setMonthlyReport(updatedMonthlyReport);
   };
 
   const isStopDisabled = !startTime;
 
   return (
-    <div className="employee-dash">
-      <h1>Employee Dashboard</h1>
-      {startTime ? (
-        <div>
-          <p>Start Time: {startTime.toLocaleString()}</p>
+    <div className="container my-4">
+      <h1 className="text-center mb-4">Employee Dashboard</h1>
+      <div className="row">
+        {startTime ? (
+          <div className="col-12 mb-3">
+            <p>Start Time: {startTime.toLocaleString()}</p>
+          </div>
+        ) : (
+          <div className="col-12 mb-3">
+            <button className="btn btn-primary" onClick={() => setStartTime(new Date())}>Start Clock</button>
+          </div>
+        )}
+        {stopTime ? (
+          <div className="col-12 mb-3">
+            <p>Stop Time: {stopTime.toLocaleString()}</p>
+          </div>
+        ) : (
+          <div className="col-12 mb-3">
+            <button className="btn btn-primary" onClick={() => setStopTime(new Date())} disabled={isStopDisabled}>
+              Stop Clock
+            </button>
+          </div>
+        )}
+  
+        <div className="col-12 mb-3">
+          <button className="btn btn-secondary" onClick={() => setShowReport(!showReport)}>View Monthly Report</button>
         </div>
-      ) : (
-        <button onClick={() => setStartTime(new Date())}>Start Clock</button>
-      )}
-      {stopTime ? (
-        <div>
-          <p>Stop Time: {stopTime.toLocaleString()}</p>
-        </div>
-      ) : (
-        <button onClick={() => setStopTime(new Date())} disabled={isStopDisabled}>
-          Stop Clock
-        </button>
-      )}
-
-      <button onClick={() => setShowReport(!showReport)}>View Monthly Report</button>
-
-      {showReport && (
-        <div>
-          <h2>Monthly Report</h2>
-          <ul>
-            {monthlyReport.map((report, reportIndex) => (
-              <li key={report.id}>
-                <h3>{report.name}</h3>
-                <ul>
-                  {report.hours.map((day, dayIndex) => (
-                    <li key={day.date.getTime()}>
-                      {day.date.toLocaleString()}:{' '}
-                      {day.start ? day.start.toLocaleString() : '-'} -{' '}
-                      {day.end ? day.end.toLocaleString() : '-'}{' '}
-                      {day.start && (
-                        <button onClick={() => handleStartEdit(reportIndex, dayIndex)}>
-                          Edit Start
-                        </button>
-                      )}
-                      {day.end && (
-                        <button onClick={() => handleStopEdit(reportIndex, dayIndex)}>
-                          Edit End
-                        </button>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+  
+        {showReport && (
+          <div className="col-12">
+            <h2 className="mt-4">Monthly Report</h2>
+            <ul className="list-unstyled mt-3">
+              {monthlyReport.map((report, reportIndex) => (
+                <li key={report.id} className="my-3">
+                  <h3>{report.name}</h3>
+                  <ul className="list-unstyled mt-3">
+                    {report.hours.map((day, dayIndex) => (
+                      <li key={day.date.getTime()} className="mb-2">
+                        {day.date.toLocaleString()}:{' '}
+                        {day.start ? day.start.toLocaleString() : '-'} -{' '}
+                        {day.end ? day.end.toLocaleString() : '-'}{' '}
+                        {day.start && (
+                          <button className="btn btn-sm btn-primary" onClick={() => handleStartEdit(reportIndex, dayIndex)}>
+                            Edit Start
+                          </button>
+                        )}
+                        {day.end && (
+                          <button className="btn btn-sm btn-primary mx-2" onClick={() => handleStopEdit(reportIndex, dayIndex)}>
+                            Edit End
+                          </button>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
